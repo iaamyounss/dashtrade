b150cd34315f1dbd6750a46064daf808c3b600a4
danmicka <mickael.dancin@gmail.com>
2022-03-08 07:41:40 +0100

    add navigation and add stats panel skeleton

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -4,6 +4,7 @@ import NavBar from "components/NavBar";
 import TypeOrdersMenu from "components/OrdersSideBar/TypeOrdersMenu";
 import CssBaseline from "@mui/material/CssBaseline";
 import SignalTable from "components/grid/SignalTable";
+import StatsPanel from "components/statsPanel/StatsPanel";
 
 const Accueil = () => {
   const [selectedToken, setSelectedToken] = React.useState("BTC/USDT");
@@ -11,13 +12,14 @@ const Accueil = () => {
   return (
     <div className="container">
       {/* <NavBar className='nav' /> */}
+
       <CssBaseline />
       <div className="central-container">
-        <SignalTable
-          onSelectedToken={setSelectedToken}
-          className="central-container-left"
-        />
-
+        <div className="central-container-left">
+          <NavBar />
+          <StatsPanel />
+          <SignalTable onSelectedToken={setSelectedToken} />
+        </div>
         <TypeOrdersMenu className="central-container-right" />
       </div>
       <OrdersPanel />
